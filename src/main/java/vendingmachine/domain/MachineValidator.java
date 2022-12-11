a8e9683be3e41843eff3b956b6820320da137185
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-11 15:09:16 +0900

    refactor: 자판기 검증기 예외 메시지 수정

--- src/main/java/vendingmachine/domain/MachineValidator.java
+++ src/main/java/vendingmachine/domain/MachineValidator.java
@@ -1,7 +1,7 @@
 package vendingmachine.domain;
 
 public class MachineValidator {
-    private static final String MACHINE_MONEY_ERROR_MESSAGE = "자판기 보유 금액은 10원 단위로 나누어 떨어져야 하며, 10원 이상입니다.";
+    private static final String MACHINE_MONEY_ERROR_MESSAGE = "금액은 10원 단위로 나누어 떨어져야 하며, 10-10000원 사이입니다.";
 
     private MachineValidator(){};
 
