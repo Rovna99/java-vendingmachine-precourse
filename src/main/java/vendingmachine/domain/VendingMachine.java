90fe3e2d08c207195ece6c542417b701ea937c90
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-11 13:53:22 +0900

    feat: 자판기 기능을 수행할 VendingMachine 생성

---
+++ src/main/java/vendingmachine/domain/VendingMachine.java
@@ -0,0 +1,20 @@
+package vendingmachine.domain;
+
+public class VendingMachine {
+
+    public void setMachineMoney() {
+
+    }
+
+    public void setProduct() {
+
+    }
+
+    public void sellProduct() {
+
+    }
+
+    public void returnChange() {
+
+    }
+}
