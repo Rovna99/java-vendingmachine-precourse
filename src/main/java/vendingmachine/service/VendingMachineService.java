03fdb01b3dafb38a49b0850b54e97be4eaaea305
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-11 14:08:24 +0900

    feat: 비즈니스 로직이 담길 VendingMachineService 생성

---
+++ src/main/java/vendingmachine/service/VendingMachineService.java
@@ -0,0 +1,28 @@
+package vendingmachine.service;
+
+import vendingmachine.domain.VendingMachine;
+
+public class VendingMachineService {
+    private final VendingMachine machine;
+
+    public VendingMachineService() {
+        machine = new VendingMachine();
+    }
+
+    public void setVendingMachineMoney() {
+
+    }
+
+
+    public void setProduct(String productDetails) {
+
+    }
+
+    public void sellProduct(String productName) {
+
+    }
+
+    public void returnChange() {
+
+    }
+}
