e6c40524cd338c5f605ca5c770db3c87af43cfb4
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-12-11 15:06:45 +0900

    feat: 자판기 보유 동전 출력 기능 구현

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,17 +1,23 @@
 package vendingmachine.view;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class OutputView {
     private static final String DEFAULT_ERROR_INTRO = "[ERROR] ";
+    private static final String CHANGES_DELIMITER = " - ";
 
 
     private OutputView(){
 
     }
 
-    public static void printChangeAmount(Map<Integer, Integer> amount) {
-
+    public static void printChangeAmount(Map<Integer, Integer> changes) {
+        for (Entry<Integer, Integer> e : changes.entrySet()) {
+            String money = e.getKey() + "원";
+            String amount = e.getValue() + "개";
+            System.out.println(String.join(CHANGES_DELIMITER, money, amount));
+        }
     }
 
     public static void printRemainMoney() {
